d872fdb6c6b6f208d59a3a6072d5ec0b16d2c9f9
masaya999 <mk28413064@outlook.jp>
2024-04-19 16:53:20 +0900

    Revise update error(partial)

--- app/app.py
+++ app/app.py
@@ -14,14 +14,7 @@ df = pd.read_csv(csv_path, encoding='cp932', index_col=0)
 file_list = sorted(set(df['file_name']))
 client_info = {}
 proposal_info = {}
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        selected_file = request.form.get('file_selector')
-        load_contents(selected_file)
-        return render_template('index.html', files=file_list, selected_file=selected_file, client_info=client_info, proposal_info=proposal_info)
-    return render_template('index.html', files=file_list, selected_file=None, client_info=None, proposal_info=None)
+selected_file = None
 
 def load_contents(file_name):
     df_tmp = df[df['file_name']==file_name]
@@ -32,12 +25,46 @@ def load_contents(file_name):
     for index, row in df_proposal.iterrows():
         proposal_info[row['label']] = row['content']
     #return client_info, proposal_info
+
+def convert_pptx_to_pdf(pptx_filename, pdf_filename):
+    prs = Presentation(pptx_filename)
+    c = canvas.Canvas(pdf_filename)
+    for slide in prs.slides:
+        c.drawString(100, 750, "Slide Title Placeholder")  # 簡単なプレースホルダー
+        c.showPage()
+    c.save()
     
-@app.route('/update', methods=['POST'])###temp!!
-def update():
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        selected_file = request.form.get('file_selector')
+        load_contents(selected_file)
+        return render_template('index.html', files=file_list, selected_file=selected_file, client_info=client_info, proposal_info=proposal_info)
+    return render_template('index.html', files=file_list, selected_file=None, client_info=None, proposal_info=None)
+
+@app.route('/files/<filename>')
+def file(filename):
+    # ファイルがPDFでなければ変換
+    base, ext = path.splitext(filename)
+    if ext.lower() == ".pptx":
+        pdf_filename = base + ".pdf"
+        convert_pptx_to_pdf(filename, pdf_filename)
+        filename = pdf_filename
+    return send_from_directory('.', filename)
+
+@app.route('/update/client', methods=['POST'])###temp!!
+def update_client():
     # テキストボックスからの新しいデータを受け取り、dictを更新
     for key in client_info.keys():
-        client_info[key] = request.get(key)
+        client_info[key] = request.form.get(key)
+    return render_template('index.html', files=file_list, selected_file=selected_file, client_info=client_info, proposal_info=proposal_info)
+
+@app.route('/update/proposal', methods=['POST'])###temp!!
+def update_proposal():
+    # テキストボックスからの新しいデータを受け取り、dictを更新
+    for key in proposal_info.keys():
+        proposal_info[key] = request.form.get(key)
+    return render_template('index.html', files=file_list, selected_file=selected_file, client_info=client_info, proposal_info=proposal_info)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
